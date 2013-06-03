f6da5a2e4b7bcffa31b1af0c4cc54e18df8ae4da
Julian Peeters <julianpeeters@gmail.com>
2013-06-02 17:41:22 -0700

    solved problem 5

--- euler002/euler002.scala
+++ euler002/euler002.scala
@@ -1,9 +1,7 @@
 
 object euler002 extends App {
   val fibs: Stream[Int] = 0 #:: 1 #:: fibs.zip(fibs.tail).map { n => n._1 + n._2 }
-  val result = fibs.takeWhile(_<4000000)
-                   .filter(_%2 == 0)
-                   .sum;
-
-    println(result)
+  //val result = fibs.takeWhile(_<4000000).filter(_%2 == 0).sum;
+    fibs.take(5).foreach(println)
+  //  println(result)
 }
--- euler005/euler005.scala
+++ euler005/euler005.scala
@@ -1,13 +1,9 @@
-
 object euler005 extends App {
 
-  val factorMax = 4
-  val factors = Stream.range(2, factorMax+1)      // Use a Stream object to create a stream of natural numbers
-               
-  val composites = factors.map(n => Stream.range(n, Integer.MAX_VALUE, n))//a stream of streams
+val naturals = Iterator.from(1)
+def factors = Iterator.from(1).take(20)
 
-  val LCM = composites.toStream.last.filter(factor => composites.forall(n => n.contains(factor)==true)).toList//.min
+val LCM = naturals.find(n => factors.forall(f => n%f == 0)).get
+  println(LCM)
 
-//  val LCM = composites.toStream.last.filter(factor => composites.forall(n => n.contains(factor)==true)).toList.min
-   // println(LCM)
 }
